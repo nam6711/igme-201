e4a9762a5d507d6980ae8e35733f64e81ac149a9
Noah <Noah@LAPTOP-FMDH9RA6>
2022-08-26 12:35:08 -0400

    idk

--- FavoriteColorandNumber/Program.cs
+++ FavoriteColorandNumber/Program.cs
@@ -8,11 +8,35 @@ namespace FavoriteColorandNumber
 {
     internal class Program
     {
+        static void PrintMyColor(string sColorString)
+        {
+            // sColorString is a local string to this method
+            // which will recieve a copy of the passed variable from the colling method
+            // append " is your favorite color" to sColorString, which will change the variable in the calling method
+            // 
+        }
+
+        static void PrintMyColorReference(ref string sColorString)
+        {
+            // sColorString is a local string to this method
+            // which will recieve a copy of the passed variable from the colling method
+            // append " is your favorite color" to sColorString, which will change the variable in the calling method
+            // 
+        }
+
         static void Main(string[] args)
         {
             string sFavColor = null;
 
             sFavColor = Console.ReadLine();
+
+            PrintMyColor(sFavColor);
+            PrintMyColorReference(ref sFavColor);
+
+            int? nFavNumber = null;
+            Console.WriteLine("What's your favorite number?");
+
+
         }
     }
 }
