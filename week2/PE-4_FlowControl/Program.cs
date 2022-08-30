dff76f328c68c934bd5ed0fb8a254748e627fe12
Noah <Noah@LAPTOP-FMDH9RA6>
2022-08-29 20:30:45 -0400

    Flow Control Program

--- week2/PE-4_FlowControl/Program.cs
+++ week2/PE-4_FlowControl/Program.cs
@@ -18,7 +18,47 @@ namespace PE_4_FlowControl
         // Restrictions: None
         static void Main(string[] args)
         {
+            // Prompt user for two numbers 
+            Console.WriteLine("Give me two numbers, one greater than 10, and one less than 10: ");
 
+            // Create two int vars to hold user inputs
+            int var1;
+            int var2;
+            string sInput1;
+            string sInput2;
+
+            // AND validate answers as real numbers
+            do
+            {
+                Console.Write("Number 1: ");
+                sInput1 = Console.ReadLine();
+            } while (!int.TryParse(sInput1, out var1));
+            do
+            {
+                Console.Write("Number 2: ");
+                sInput2 = Console.ReadLine();
+            } while (!int.TryParse(sInput2, out var2));
+
+            // Prompt user for new inputs if the check fails
+            // Check to see if only one is greater than 10
+            while ((var1 > 10 && var2 > 10) || (var1 < 10 && var2 < 10))
+            {
+                Console.WriteLine("Invalid Answer, please give me ONE number GREATER THAN 10 and ONE number LESS THAN 10: ");
+                // validate answers as real numbers
+                do
+                {
+                    Console.Write("Number 1: ");
+                    sInput1 = Console.ReadLine();
+                } while (!int.TryParse(sInput1, out var1));
+                do
+                {
+                    Console.Write("Number 2: ");
+                    sInput2 = Console.ReadLine();
+                } while (!int.TryParse(sInput2, out var2));
+            }
+
+            //Print a success message
+            Console.WriteLine($"You input {var1} and {var2}! One is less than 10, and one is greater than 10!");
         }
     }
 }
