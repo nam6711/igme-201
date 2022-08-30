0620419ffce5a5284c9fe69ed8dd3eac271b8acb
Noah <Noah@LAPTOP-FMDH9RA6>
2022-08-29 20:13:14 -0400

    Changed File Management

---
+++ week2/PE-4_FlowControl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_4_FlowControl
+{
+    // Class: Program
+    // Author: Noah Manoucheri
+    // Restrictions: Code Snippets
+    internal class Program
+    {
+        // Method: Main
+        // Purpose: Take two inputs from the user (intigers) and compare them
+        //          to see if they're both greater than 10. If only ONE is greater
+        //          than 10, print a end program. Otherwise, prompt the user again
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
