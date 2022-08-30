bbc4411abdb62f336cb711a9ff6cdf6c650a57e2
Noah <Noah@LAPTOP-FMDH9RA6>
2022-08-29 21:15:56 -0400

    finalized the user inputs for the Mandelbrot

--- week2/Mandlebrot/Program.cs
+++ week2/Mandlebrot/Program.cs
@@ -22,11 +22,28 @@ namespace Mandelbrot
         static void Main(string[] args)
         {
             double realCoord, imagCoord;
+            double nStartRealCoord, nStartImagCoord; // used to hold the initial position of the Mandelbrot
+            string sRealCoord, sImagCoord; //strings to hold the users inputs for the Mandelbrot
             double realTemp, imagTemp, realTemp2, arg;
-            int iterations;
-            for (imagCoord = 1.2; imagCoord >= -1.2; imagCoord -= 0.05)
+            int iterations
+
+            //Prompt and Validate the realCoord from user
+            Console.WriteLine("Please input a start X coordinate for the Mandelbrot!\nThe normal start position is -0.6: ");
+            do
             {
-                for (realCoord = -0.6; realCoord <= 1.77; realCoord += 0.03)
+                sRealCoord = Console.ReadLine();
+            } while (!double.TryParse(sRealCoord, out nStartRealCoord));
+
+            //Prompt and Validate the imagCoord from user
+            Console.WriteLine("Please input a start Y coordinate for the Mandelbrot!\nThe normal start position is 1.2: ");
+            do
+            {
+                sImagCoord = Console.ReadLine();
+            } while (!double.TryParse(sImagCoord, out nStartImagCoord));
+
+            for (imagCoord = nStartImagCoord; imagCoord >= nStartImagCoord - (0.05 * 48); imagCoord -= 0.05)
+            {
+                for (realCoord = nStartRealCoord; realCoord <= nStartRealCoord + (0.03 * 80); realCoord += 0.03)
                 {
                     iterations = 0;
                     realTemp = realCoord;
