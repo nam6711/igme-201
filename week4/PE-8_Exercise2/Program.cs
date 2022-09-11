936831fd0c46b55a18cfa751af840433b0d22c4c
Noah <Noah@LAPTOP-FMDH9RA6>
2022-09-11 12:47:33 -0400

    added functionality

--- week4/PE-8_Exercise2/Program.cs
+++ week4/PE-8_Exercise2/Program.cs
@@ -10,6 +10,22 @@ namespace PE_8_Exercise2
     {
         static void Main(string[] args)
         {
+            // prompt user for a string
+            Console.WriteLine("Give me a string");
+            string sInput = Console.ReadLine();
+
+            // create a string to hold the reversed string order
+            string sReverse = "";
+
+            // for-loop that goes reverse order through a string
+            for (int i = sInput.Length; i > 0; i--)
+            {
+                // save the current string character onto the temp var
+                sReverse += sInput[i - 1];
+            }
+
+            // print the var
+            Console.WriteLine(sReverse);
         }
     }
 }
