1bcd6509a6a2dcbc4cb87b70bcd5520b45a3aa21
Noah <Noah@LAPTOP-FMDH9RA6>
2022-09-25 20:09:08 -0400

    initial commit

---
+++ Test1/Test1_Question3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Question3
+{
+    internal class Program
+    {
+        delegate double Rounder( double input , int decimals );
+
+        static void Main(string[] args)
+        {
+            //holds delegate function for Math.Round
+            Rounder round = new Rounder(Math.Round);
+
+            //holds the number to be a double
+            double dInput;
+
+            Func<string, bool> CheckValid = input =>
+            {
+                if (double.TryParse(input, out dInput))
+                {
+                    return true;
+                } 
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+}
