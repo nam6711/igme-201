05e9fd07158ed56d28ff8e3db161c049fa9ee511
Noah <Noah@LAPTOP-FMDH9RA6>
2022-09-25 21:59:30 -0400

    initial commit and added struct functionality

---
+++ Test1/Test1_Question13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Question12
+{
+    internal class Program
+    {
+        struct employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+
+        static void Main(string[] args)
+        {
+            //vairable declaration
+            employee worker;
+            worker.dSalary = 30000;
+
+            //ask for name
+            Console.WriteLine("What's your name? ");
+            worker.sName = Console.ReadLine();
+
+            if (GiveRaise(worker.sName, ref worker.dSalary))
+            {
+                Console.WriteLine("Congrats on the raise {0}! Your salary is now: {1}!", worker.sName, worker.dSalary);
+            }
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            // if name == "Nat", give them a raise, otherwise don't
+            if (string.Equals(name, "Nat"))
+            {
+                salary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
