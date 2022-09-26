8849ebc2fb01d98ba43a15a5123be8966c5746d8
Noah <Noah@LAPTOP-FMDH9RA6>
2022-09-25 21:56:06 -0400

    added functionality

--- Test1/Test1_Question12/Program.cs
+++ Test1/Test1_Question12/Program.cs
@@ -10,6 +10,32 @@ namespace Test1_Question12
     {
         static void Main(string[] args)
         {
+            //vairable declaration
+            string sName;
+            double dSalary = 30000;
+
+            //ask for name
+            Console.WriteLine("What's your name? ");
+            sName = Console.ReadLine();
+
+            if (GiveRaise(sName, ref dSalary))
+            {
+                Console.WriteLine("Congrats on the raise {0}! Your salary is now: {1}!", sName, dSalary);
+            }
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            // if name == "Nat", give them a raise, otherwise don't
+            if (string.Equals(name, "Nat"))
+            {
+                salary += 19999.99;
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
         }
     }
 }
