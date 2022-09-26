9ff06c6b3c4de6e78fbe703215644f3916e057b7
Noah <Noah@LAPTOP-FMDH9RA6>
2022-09-25 20:22:24 -0400

    Added functionality

--- Test1/Test1_Question3/Program.cs
+++ Test1/Test1_Question3/Program.cs
@@ -13,11 +13,17 @@ namespace Test1_Question3
         static void Main(string[] args)
         {
             //holds delegate function for Math.Round
-            Rounder round = new Rounder(Math.Round);
+            Rounder round = delegate (double input, int decimals) {
+                return Math.Round(input, decimals);
+            };
 
             //holds the number to be a double
-            double dInput;
+            double dInput = 0;
+            // take input
+            Console.WriteLine("Please input a double, we'll round it to the second decimal place");
+            string sInput = Console.ReadLine();
 
+            // function is used to validate an input
             Func<string, bool> CheckValid = input =>
             {
                 if (double.TryParse(input, out dInput))
@@ -29,6 +35,18 @@ namespace Test1_Question3
                     return false;
                 }
             };
+
+            // while input isn't valid revalidate it
+            while (!CheckValid(sInput))
+            {
+                Console.WriteLine("Please input a valid double number");
+                sInput = Console.ReadLine();
+            }
+
+            // round the number using a delegate
+            dInput = round(dInput, 2);
+            // print out the number rounded to the second decimal place
+            Console.WriteLine(dInput);
         }
     }
 }
