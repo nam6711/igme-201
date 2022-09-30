5e4d9a355b9cfd32af4bccad6ef05102a9d95b9b
Noah <Noah@LAPTOP-FMDH9RA6>
2022-09-29 22:10:24 -0400

    added classes

--- week6/Vehicles/Class1.cs
+++ week6/Vehicles/Class1.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace Vehicles
 {
-    public class Class1
+    public abstract class Vehicle
     {
+        public virtual void LoadPassenger()
+        {
+            // code to be implimented
+        }
     }
+
+    public abstract class Car : Vehicle
+    {
+        // code to impliment
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface IHeaveyLoadCarrier
+    {
+        // code to impliment
+    }
+
+    public abstract class Train : Vehicle
+    {
+        // code to impliment
+    }
+
+    public class Compact : Car, IPassengerCarrier
+    {
+        // code to impliment
+    }
+
+    public class SUV : Car, IPassengerCarrier
+    {
+        // code to impliment
+    }
+
+    public class Pickup : Car, IPassengerCarrier, IHeaveyLoadCarrier
+    {
+        // code to impliment
+    }
+
+    public class PassengerTrain : Train, IPassengerCarrier
+    {
+        // code to impliment
+    }
+
+    public class FreighTrain : Train, IHeaveyLoadCarrier
+    {
+        // code to impliment
+    }
+
+    public class _424DoubleBogey : Train, IHeaveyLoadCarrier
+    {
+        // code to impliment
+    }
+
 }
