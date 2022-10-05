39c0a5676f8dc7bf3d6876408aa7a06c602e62f4
Noah <Noah@LAPTOP-FMDH9RA6>
2022-10-05 12:35:01 -0400

    initial commit

---
+++ week7/GameOfLife/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal class Program
+    {
+        public enum EAliveState
+        {
+            alive,
+            dead,
+        }
+
+        public enum EInfectedState
+        {
+            organic,
+            infected,
+            vaccinated
+        }
+
+        public enum EDirection
+        {
+            right,
+            down,
+            left,
+            up,
+            topleft,
+            topright,
+            bottomleft,
+            bottomright,
+        }
+
+        public struct StructCellSate
+        {
+            public EInfectedState infectedState;
+
+            private EAliveState aliveState;
+
+            private EAliveState AliveState { 
+                get
+                {
+                    return this.aliveState;
+                }
+                set
+                {
+                    this.aliveState = value;
+
+                    if (this.aliveState == EAliveState.dead)
+                    {
+                        this.infectedState = EInfectedState.organic;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
