4957492e9e4adb611fcf52f6758bb49859c87789
nmanoucheri001 <nmanoucheri001@idtech.com>
2022-10-08 12:58:07 -0400

    initial commits

---
+++ week6/SongLib/SongLib/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongLib
+{
+    public abstract class Song
+    {
+        public int year;
+        public string lyrics;
+        public string composer;
+        public string artist;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public abstract void Play();
+
+        public virtual void Copy()
+        {
+
+        }
+
+        public void Dance()
+        {
+            
+        }
+
+        public void Sing()
+        {
+
+        }
+    }
+
+    public class TapeSong : Song
+    {
+
+    }
+}
