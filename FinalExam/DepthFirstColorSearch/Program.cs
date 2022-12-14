0475292899fc355ad9e97098886f29720da6a262
Noah <Noah@LAPTOP-FMDH9RA6>
2022-12-14 16:24:16 -0500

    Final Commit for the DFS

--- FinalExam/DepthFirstColorSearch/Program.cs
+++ FinalExam/DepthFirstColorSearch/Program.cs
@@ -22,7 +22,7 @@ namespace DepthFirstColorSearch
     {
         // graph of all connected colors. if a color does not have an edge with another
         //      it is denoted with -1. All other have real numbers
-        static int[,] colorGraph = new[,]
+        static int[,] colorMGraph = new[,]
         {
                       /* red blue yellow cyan gray purple orange green */
             /* red */   {-1,   1,   -1,  -1,  5,   -1,    -1,    -1 },
@@ -61,8 +61,66 @@ namespace DepthFirstColorSearch
             /* green */ null
         };
 
+        // Method : DepthFirstSearch
+        // Author : David Schuh,
+        //              adapted into EColor format by Nat Manoucheri
+        // Purpose : Calls the utility method for the Depth First Search
+        //              on initialization with the given start color
+        // Parameters : EColor color - represents the start root for the DFS
+        // Returns : void
+        static void DepthFirstSearch(EColor eColor)
+        {
+            // array of all nodes on the graph we've covered already
+            bool[] visited = new bool[colorAGraph.Length];
+
+            // send in the state of the enum EColors we are on 
+            //   and/or currently searching as well as the array of visitied
+            //   points
+            DepthFirstSearchUtility(eColor, visited);
+        }
+
+        // Method : DepthFirstSearchUtility
+        // Author : David Schuh,
+        //              adapted into EColor format by Nat Manoucheri
+        // Purpose : Runs through an adjacency matrix so we can view all
+        //              adjacent edges stemming from the given root
+        // Parameters : EColor v - represents the current node on the tree we
+        //                  are on and intend to traverse
+        //              bool[] visited - holds which nodes we've already visited into
+        //                   an array of colorAGraph.Length
+        // Returns : void
+        static void DepthFirstSearchUtility(EColor v, bool[] visited)
+        {
+            // set the current color to having been visited so we don't
+            //      traverse it again on another node
+            visited[(int)v] = true;
+
+            // Output the node we are on so we can view the path of the app
+            Console.WriteLine(v.ToString());
+
+            // set the current color to traverse to the graph position of the associated
+            //      color
+            int[] currentColorList = colorAGraph[(int)v];
+            if (currentColorList != null)
+            {
+                // Run through all the adjacent nodes to this current one, and view
+                //      if we've visited each.
+                foreach (int n in currentColorList)
+                {
+                    // if we haven't visited this next node yet, 
+                    //      then run the Utility again and visit all of its nodes too
+                    if (!visited[n])
+                    {
+                        DepthFirstSearchUtility((EColor)n, visited);
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Perform a DFS Search of the color red
+            DepthFirstSearch(EColor.red);
         }
     }
 }
