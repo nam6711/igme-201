d00212636f7360835c9d5ba4ca5af3222e6ead6e
Noah <Noah@LAPTOP-FMDH9RA6>
2022-12-14 15:36:39 -0500

    Added the stack

---
+++ FinalExam/MyStack/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    public class MyStack
+    {
+        public List<int> myStack = new List<int>();
+    
+        public void Push(int n)
+        {
+            myStack.Add(n);
+        }
+
+        public int? Peek()
+        {
+            if (myStack.Count > 0)
+            {
+                return myStack[myStack.Count - 1];
+            } 
+            else
+            {
+                return null;
+            }
+        }
+
+        public int? Pop()
+        {
+            if (myStack.Count > 0)
+            {
+                int? n = myStack[myStack.Count - 1];
+                myStack.RemoveAt(myStack.Count - 1); 
+                return n;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
